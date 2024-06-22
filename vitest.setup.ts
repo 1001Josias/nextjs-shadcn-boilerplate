02c95ca7f6465c0e1bb0f880a038248a09b04f7a
1001josias <1001josias@gmail.com>
2024-06-22 11:44:44 -0300

    test: theme provider

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,22 @@
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import 'jsdom'
+import '@testing-library/jest-dom'
+
+beforeEach(() => {
+  const mockMatchMedia = vi.fn().mockImplementation((query: string) => {
+    return {
+      matches: false,
+      media: query,
+      onchange: null,
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    }
+  })
+  window.matchMedia = mockMatchMedia
+})
 
 afterEach(() => {
   cleanup()
