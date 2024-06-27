85945542034557c5ec663a9b184d8e99cb2421b5
1001josias <1001josias@gmail.com>
2024-06-26 22:28:16 -0300

    feat: home page

---
+++ src/app/page.config.ts
@@ -0,0 +1,7 @@
+import { year } from '@/lib/date'
+
+export const CONFIG = {
+  title: `Welcome to Best Boilerplate in ${year}`,
+  description: 'A Next.js boilerplate with Shadcn, TypeScript, Tailwind and more!',
+  github: 'https://github.com/1001Josias/nextjs-shadcn-boilerplate',
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image'
-
+import Button from '@/components/Button'
+import Link from 'next/link'
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  return (
+    <>
+      <main className="flex flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center space-y-8">
+          <h1 className="text-4xl font-bold tracking-tight text-center">Welcome to Best Boilerplate in 2024</h1>
+          <Link href={'/about'}>
+            <Button variant={'default'}>See more</Button>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
 }
